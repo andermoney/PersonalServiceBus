d0227129aabecba49e5762f8c20f230cdb3c2689
Jake Anderson <andermoney@gmail.com>
2013-03-19 21:50:49 -0500

    Branch to rebuild without solution builder

---
+++ PersonalServiceBus.RSS/Infrastructure/GeneratedCode/Videos/GetVideoSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Config;
+using PersonalServiceBus.InternalMessages.Videos;
+
+namespace PersonalServiceBus.RSS.Components.Videos
+{
+    public partial class GetVideoSender: IGetVideoSender, PersonalServiceBus.RSS.Infrastructure.INServiceBusComponent
+    {
+        public void Send(GetVideo message)
+		{
+			Bus.Send(message);	
+		}
+
+        public IBus Bus { get; set; }
+    }
+
+    public interface IGetVideoSender
+    {
+        void Send(GetVideo message);
+    }
+}
