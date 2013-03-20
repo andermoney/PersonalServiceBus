5997aa4f59cefeaf8239757828b7b76750bb656c
Jake Anderson <andermoney@gmail.com>
2013-03-19 23:33:42 -0500

    More changes to try and rebuild solution

--- PersonalServiceBus.InternalMessages/Videos/GetVideo.cs
+++ PersonalServiceBus.InternalMessages/Videos/GetVideo.cs
@@ -1,7 +1,22 @@
-﻿namespace PersonalServiceBus.InternalMessages.Videos
+﻿using System;
+using NServiceBus;
+using NServiceBus.Config;
+using PersonalServiceBus.InternalMessages.Videos;
+
+namespace PersonalServiceBus.RSS1.Components.Videos
 {
-    public class GetVideo
+    public partial class GetVideoSender: IGetVideoSender, PersonalServiceBus.RSS1.Infrastructure.INServiceBusComponent
     {
-         
+        public void Send(GetVideo message)
+		{
+			Bus.Send(message);	
+		}
+
+        public IBus Bus { get; set; }
+    }
+
+    public interface IGetVideoSender
+    {
+        void Send(GetVideo message);
     }
 }
