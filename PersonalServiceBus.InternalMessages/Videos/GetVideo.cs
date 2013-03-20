3815caf0e46ae49ebaf4cef8490428b1690c1833
Jake Anderson <andermoney@gmail.com>
2013-03-19 23:40:20 -0500

    Tried removing all RSS projects

--- PersonalServiceBus.InternalMessages/Videos/GetVideo.cs
+++ PersonalServiceBus.InternalMessages/Videos/GetVideo.cs
@@ -3,9 +3,9 @@ using NServiceBus;
 using NServiceBus.Config;
 using PersonalServiceBus.InternalMessages.Videos;
 
-namespace PersonalServiceBus.RSS1.Components.Videos
+namespace PersonalServiceBus.RSS.Components.Videos
 {
-    public partial class GetVideoSender: IGetVideoSender, PersonalServiceBus.RSS1.Infrastructure.INServiceBusComponent
+    public partial class GetVideoSender: IGetVideoSender, PersonalServiceBus.RSS.Infrastructure.INServiceBusComponent
     {
         public void Send(GetVideo message)
 		{
