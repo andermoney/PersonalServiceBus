a27ee1cee3a1ae7a63f0cc217689eeabf0123f06
Jake Anderson <andermoney@gmail.com>
2013-04-02 00:18:58 -0500

    Started to add functionality to add feeds

--- PersonalServiceBus.RSS/SignalR/FeedHub.cs
+++ PersonalServiceBus.RSS/SignalR/FeedHub.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using NServiceBus;
+using PersonalServiceBus.RSS.Core.Domain.Interface;
+using PersonalServiceBus.RSS.Core.Domain.Model;
+using PersonalServiceBus.RSS.Infrastructure;
 using PersonalServiceBus.RSS.Models;
 
 namespace PersonalServiceBus.RSS.SignalR
 {
     [HubName("feedHub")]
-    public class FeedHub : Hub
+    public class FeedHub : Hub, INServiceBusComponent
     {
+        public IBus Bus { get; set; }
+
+        public IFeedManager FeedManager { get; set; }
+
         public IEnumerable<Category> GetFeedCategories()
         {
             return new List<Category>
@@ -18,6 +26,11 @@ namespace PersonalServiceBus.RSS.SignalR
                             CategoryName = "Webcomics"
                         }
                 };
+        }
+
+        public void AddFeed(Feed feed)
+        {
+            var status = FeedManager.AddFeed(feed);
         }
     }
 }
