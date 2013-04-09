40c022d62b66eb1b8f8770276619891c8ac6bfc5
Jake Anderson <andermoney@gmail.com>
2013-04-08 20:04:17 -0500

    Try to get require JS working with SignalR

--- PersonalServiceBus.RSS/Scripts/feedHub.js
+++ PersonalServiceBus.RSS/Scripts/feedHub.js
@@ -1,46 +1,57 @@
-﻿// Crockford's supplant method (poor man's templating)
-if (!String.prototype.supplant) {
-    String.prototype.supplant = function (o) {
-        return this.replace(/{([^{}]*)}/g,
-            function (a, b) {
-                var r = o[b];
-                return typeof r === 'string' || typeof r === 'number' ? r : a;
-            }
-        );
-    };
-}
-
-$(function () {
-    var feedHub = $.connection.feedHub,
-        $categoryList = $('#feed-category-list'),
-        feedCategoryTemplate = '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#feed-category-list" href="#{CategoryId}">{CategoryName}</a></div><div id="{CategoryId}" class="accordion-body collapse"><div class="accordion-inner">Feeds here...</div></div></div>';
-
-    function formatCategory(category) {
-        return $.extend(category, {
-
-        });
+﻿require(['/Scripts/jquery.signalR-1.0.1.min.js', '/signalr/hubs'], function (signalR, hubs) {
+    // Crockford's supplant method (poor man's templating)
+    if (!String.prototype.supplant) {
+        String.prototype.supplant = function (o) {
+            return this.replace(/{([^{}]*)}/g,
+                function (a, b) {
+                    var r = o[b];
+                    return typeof r === 'string' || typeof r === 'number' ? r : a;
+                }
+            );
+        };
     }
 
-    function init() {
-        return feedHub.server.getFeedCategories().done(function (categories) {
-            $.each(categories, function () {
-                var category = formatCategory(this);
-                $categoryList.append(feedCategoryTemplate.supplant(category));
+    function createHub() {
+        var feedHub = $.connection.feedHub,
+            $categoryList = $('#feed-category-list'),
+            feedCategoryTemplate = '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#feed-category-list" href="#{CategoryId}">{CategoryName}</a></div><div id="{CategoryId}" class="accordion-body collapse"><div class="accordion-inner">Feeds here...</div></div></div>';
+
+        function formatCategory(category) {
+            return $.extend(category, {
+
             });
-        });
+        }
+
+        function getFeedCategories() {
+            feedHub.server.getFeedCategories().done(function (categories) {
+                $.each(categories, function () {
+                    var category = formatCategory(this);
+                    $categoryList.append(feedCategoryTemplate.supplant(category));
+                });
+            });
+        }
+
+        function setupClient() {
+            //client methods the server will call back
+            $.extend(feedHub.client, {
+                updateCategoryFeeds: function (category) {
+                    var $category = $('#' + category.CategoryId + ' .accordion-inner');
+                    $category.html('Feed count updated.');
+                }
+            });
+        }
+
+        //start the connection
+        $.connection.hub.start({ waitForPageLoad: false })
+            .done(function () {
+                setupClient();
+                $(document).trigger('hubStarted');
+                getFeedCategories();
+            });
+        return feedHub;
     }
 
-    //client methods the server will call back
-    $.extend(feedHub.client, {
-        updateCategoryFeeds: function (category) {
-            var $category = $('#' + category.CategoryId + ' .accordion-inner');
-            $category.html('Feed count updated.');
-        }
-    });
-
-    //start the connection
-    $.connection.hub.start()
-        .pipe(init)
-        .done(function () {
-        });
+    return {
+        hub: createHub()
+    };
 });
---
+++ PersonalServiceBus.RSS/Scripts/main.js
@@ -0,0 +1,15 @@
+﻿requirejs([], function() {
+    require.config({
+        paths: {
+            jquery: '/Scripts/jquery-1.9.1.min'
+        },
+        shims: {
+            "/Scripts/jquery.signalR-1.0.1.min": {
+                deps: ['jquery']
+            },
+            "/signalr/hubs": {
+                deps: ['jquery', 'jquery.signalR-1.0.1.min']
+            }
+        }
+    });
+});
