521934e6612557446a13a4e0f2ec16c5c331c531
Jake Anderson <andermoney@gmail.com>
2013-04-10 21:39:16 -0500

    Show newly added feeds immediately in the UI

--- PersonalServiceBus.RSS/Scripts/feedHub.js
+++ PersonalServiceBus.RSS/Scripts/feedHub.js
@@ -18,34 +18,14 @@
             .animate({ backgroundColor: current }, duration / 2);
     };
 
+    function formatFeed(feed) {
+        return $.extend(feed, {
+            Id: feed.Id.replace('/', '-')
+        });
+    }
+
     function createHub() {
-        var feedHub = $.connection.feedHub,
-            $categoryList = $('#feed-category-list'),
-            feedCategoryTemplate = '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#feed-category-list" href="#{Id}">{Name}</a></div><div id="{Id}" class="accordion-body collapse"><div class="accordion-inner"><ul class="nav nav-pills nav-stacked"></ul></div></div></div>',
-            feedTemplate = '<li id="{Id}"><a href="#">{Name} <span class="badge badge-info">{UnreadCount}</span></a></li>';
-
-        function formatFeed(feed) {
-            return $.extend(feed, {
-                Id: feed.Id.replace('/', '-')
-            });
-        }
-
-        function addFeed(feed, newFeed) {
-            var $category = $('#' + feed.Category + ' .accordion-inner ul', $categoryList),
-                $feed;
-            if ($category.length == 0) {
-                var category = {
-                    Id: feed.Category,
-                    Name: feed.Category
-                };
-                $categoryList.append(feedCategoryTemplate.supplant(category));
-                $category = $('#' + feed.Category + ' .accordion-inner ul', $categoryList);
-            }
-            $feed = $('#' + feed.Id, $category);
-            if ($feed.length == 0) {
-                $category.append(feedTemplate.supplant(feed));
-            }
-        }
+        var feedHub = $.connection.feedHub;
 
         function getFeeds() {
             feedHub.server.getFeeds().done(function (feedResponse) {
@@ -96,8 +76,32 @@
         }
     }
 
+    function addFeed(feed, newFeed) {
+        var $categoryList = $('#feed-category-list'),
+            feedCategoryTemplate = '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#feed-category-list" href="#{Id}">{Name}</a></div><div id="{Id}" class="accordion-body collapse"><div class="accordion-inner"><ul class="nav nav-pills nav-stacked"></ul></div></div></div>',
+            feedTemplate = '<li id="{Id}"><a href="#">{Name} <span class="badge badge-info">{UnreadCount}</span></a></li>',
+            $category = $('#' + feed.Category + ' .accordion-inner ul', $categoryList),
+            $feed;
+
+        feed = formatFeed(feed);
+
+        if ($category.length == 0) {
+            var category = {
+                Id: feed.Category,
+                Name: feed.Category
+            };
+            $categoryList.append(feedCategoryTemplate.supplant(category));
+            $category = $('#' + feed.Category + ' .accordion-inner ul', $categoryList);
+        }
+        $feed = $('#' + feed.Id, $category);
+        if ($feed.length == 0) {
+            $category.append(feedTemplate.supplant(feed));
+        }
+    }
+
     return {
         hub: createHub(),
-        showError: showError
+        showError: showError,
+        addFeed: addFeed
     };
 });
