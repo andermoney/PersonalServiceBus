f84dd27bac79778297ea0b46d31f88c3abaebf84
Jake Anderson <andermoney@gmail.com>
2013-04-11 23:27:32 -0500

    Clear subscribe form after adding feed successfully

---
+++ PersonalServiceBus.RSS/Scripts/formHelper.js
@@ -0,0 +1,26 @@
+﻿define([], function () {
+    function getFormData($elem) {
+        var data = {};
+        $('input', $elem).each(function () {
+            var name = $(this).attr('name');
+            if (name) {
+                data[name] = $(this).val();
+            }
+        });
+        return data;
+    }
+    
+    function clearForm($elem) {
+        $('input', $elem).each(function () {
+            var name = $(this).attr('name');
+            if (name) {
+                $(this).val('');
+            }
+        });
+    }
+
+    return {
+        getFormData: getFormData,
+        clearForm: clearForm
+    };
+});
