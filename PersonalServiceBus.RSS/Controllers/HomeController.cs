7156f16be21ee461868bc3cbe2af1e36868ed26d
Jake Anderson <andermoney@gmail.com>
2013-04-16 23:48:41 -0500

    Remove unneeded views Force authentication to view feeds

--- PersonalServiceBus.RSS/Controllers/HomeController.cs
+++ PersonalServiceBus.RSS/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using System.Web.Mvc;
 using PersonalServiceBus.RSS.Components.Feeds;
 using PersonalServiceBus.RSS.Components.Videos;
-using PersonalServiceBus.RSS.Messages.Feeds;
 using PersonalServiceBus.RSS.Messages.Videos;
-using PersonalServiceBus.RSS.Models;
 
 namespace PersonalServiceBus.RSS.Controllers
 {
@@ -12,6 +10,7 @@ namespace PersonalServiceBus.RSS.Controllers
         public IGetVideoSender GetVideoSender { get; set; }
         public IAddFeedSender AddFeedSender { get; set; }
 
+        [Authorize]
         public ActionResult Index()
         {
             return View();
@@ -26,32 +25,6 @@ namespace PersonalServiceBus.RSS.Controllers
         {
             GetVideoSender.Send(new GetVideo());
             return RedirectToAction("Index");
-        }
-
-        public ActionResult AddFeed()
-        {
-            return View();
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        [AsyncTimeout(50000)]
-        public void AddFeedAsync(AddFeedViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return;
-
-            var response = AddFeedSender.Send(new AddFeed());
-
-            AsyncManager.Parameters["errorMessage"] = response.ErrorMessage;
-        }
-
-        public ActionResult AddFeedCompleted(string errorMessage)
-        {
-            if (!ModelState.IsValid)
-                return View();
-
-            ViewBag.ErrorMessage = errorMessage;
-            return RedirectToAction("Index");            
         }
     }
 }
