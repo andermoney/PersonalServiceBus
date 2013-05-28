dd1b5c91e2d34143c3dde44a93c552e6ee36d471
Jake Anderson <andermoney@gmail.com>
2013-05-27 21:57:07 -0500

    Added testing and OpenCover to get an idea of what needs to be covered for testing

---
+++ PersonalServiceBus.RSS.Test.Unit/FeedManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using PersonalServiceBus.RSS.Core.Domain.Enum;
+using PersonalServiceBus.RSS.Core.Domain.Interface;
+using PersonalServiceBus.RSS.Core.Domain.Model;
+using PersonalServiceBus.RSS.Infrastructure.RavenDB;
+
+namespace PersonalServiceBus.RSS.Test.Unit
+{
+    [TestFixture]
+    public class FeedManagerTest
+    {
+        [Test]
+        public void AddFeedTest()
+        {
+            //Arrange
+            var configuration = new Mock<IConfiguration>();
+            IDatabase database = new RavenDatabase(configuration.Object);
+            IFeedManager feedManager = new FeedManager(database);
+
+            //Act
+            Feed feed = new Feed();
+            var response = feedManager.AddFeed(feed);
+
+            //Assert
+            Assert.AreEqual(ErrorLevel.None, response.Status.ErrorLevel);
+        }
+    }
+}
