dbe10dd727ee03c88f0cb3526692d4363de3009d
Jake Anderson <andermoney@gmail.com>
2013-05-27 22:46:47 -0500

    Update coverage script for detailed reporting

--- opencover.ps1
+++ opencover.ps1
@@ -1,3 +1,3 @@
 .\packages\OpenCover.4.5.1604\OpenCover.Console.exe -register:user -target:.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe -targetargs:"PersonalServiceBus.RSS.Test.Unit.dll /noshadow" "-filter:+[PersonalServiceBus.RSS.*]* -[PersonalServiceBus.*.Test*]*" -output:PersonalServiceBus.RSS.Test.output.xml -targetdir:.\PersonalServiceBus.RSS.Test.Unit\bin\Debug\
-.\packages\ReportGenerator.1.8.1.0\ReportGenerator.exe -reports:PersonalServiceBus.RSS.Test.output.xml -reporttypes:HtmlSummary -targetdir:.
-.\index.htm
+.\packages\ReportGenerator.1.8.1.0\ReportGenerator.exe -reports:PersonalServiceBus.RSS.Test.output.xml -reporttypes:Html -targetdir:.\TestReports
+.\TestReports\index.htm
