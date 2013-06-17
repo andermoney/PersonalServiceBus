c47303bc8898186bcc47aeed50219d864a6babd7
Jake Anderson <andermoney@gmail.com>
2013-06-17 14:01:23 -0500

    Add RSS tests and removed unused category model

--- PersonalServiceBus.RSS.Core/Domain/Model/Category.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PersonalServiceBus.RSS.Core.Domain.Model
-{
-    public class Category : EntityBase
-    {
-        public string Name { get; set; }
-    }
-}
---
+++ PersonalServiceBus.RSS.Test.Unit/RssManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using PersonalServiceBus.RSS.Core.Domain.Enum;
+using PersonalServiceBus.RSS.Core.Domain.Interface;
+using PersonalServiceBus.RSS.Core.Domain.Model;
+using PersonalServiceBus.RSS.Infrastructure.RSS;
+
+namespace PersonalServiceBus.RSS.Test.Unit
+{
+    [TestFixture]
+    public class RssManagerTest
+    {
+        [Test]
+        public void GetFeedItemsTest()
+        {
+            //Arrange
+            IRssManager rssManager = new RssManager();
+
+            //Act
+            var feedItems = rssManager.GetFeedItems(new Feed
+                {
+                    Url = "http://feeds.feedburner.com/stereogum/cBYa"
+                });
+
+            //Assert
+            Assert.IsNotNull(feedItems);
+            Assert.AreEqual(ErrorLevel.None, feedItems.Status.ErrorLevel, feedItems.Status.ErrorMessage);
+            Assert.IsNotNull(feedItems.Data);
+            Assert.IsNotEmpty(feedItems.Data);
+        }
+    }
+}
