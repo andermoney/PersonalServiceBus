004b473faaac89962ab7d8fd3c059b63cbf41ee2
Jake Anderson <andermoney@gmail.com>
2014-08-06 06:58:10 -0500

    Upgrade project and clean up some errors

---
+++ PersonalServiceBus.RSS/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PersonalServiceBus.RSS.App_Start.Startup))]
+
+namespace PersonalServiceBus.RSS.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
